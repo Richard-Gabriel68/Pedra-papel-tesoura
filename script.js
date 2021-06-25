ee8e51b3ca9db209a66fdb6e715e8061d169c728
Richard Gabriel <rickanimals02@gmail.com>
2021-06-25 19:34:23 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function start(n)
+{
+    let pedra = document.querySelector('.pe');
+    let papel = document.querySelector('.pa');
+    let tesoura = document.querySelector('te');
+    let lista = [pedra, papel, tesoura];
+    let rival = '';
+
+    if (n === 1)
+    {
+        rival = lista.sort();
+    } 
+
+
+
+}
