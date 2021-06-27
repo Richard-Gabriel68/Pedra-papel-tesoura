9babb10ca1e6c8e48c874f495949d1367367ea02
Richard Gabriel <rickanimals02@gmail.com>
2021-06-27 15:16:53 -0300

    Adicionanco sistema de pontuação

--- script.js
+++ script.js
@@ -3,14 +3,82 @@ function start(n)
     let pedra = document.querySelector('.pe');
     let papel = document.querySelector('.pa');
     let tesoura = document.querySelector('te');
+    let scoreBot = document.querySelector('.scoreBot');
+    let scorePlayer = document.querySelector('.scorePlayer');
     let lista = [pedra, papel, tesoura];
-    let rival = '';
+    let rival = lista[Math.floor(Math.random() * lista.length)];
+    let pontosB = 0;
+    let pontosP = 0;
 
-    if (n === 1)
+    if (n === 1) //Pedra
     {
-        rival = lista.sort();
+        switch (rival)
+        {
+            case pedra:
+                console.log('Empate!');
+                pontosB++;
+                pontosP++;
+                scoreBot.innerHTML = `Pontuação da Maqinha: ${pontosB}`;
+                scorePlayer.innerHTML = `Sua pontuação: ${pontosP}`;
+            break;
+            case papel:
+                console.log('Perdeu!');
+                pontosB++;
+                scoreBot.innerHTML = `Pontuação da Maqinha: ${pontosB}`;
+            break;
+            case tesoura:
+                console.log('Ganhou!!');
+                pontosP++;
+                scorePlayer.innerHTML = `Sua pontuação: ${pontosP}`;
+            break;
+        }
+
     } 
-
-
+    else if (n === 2) //Papel
+    {
+        switch (rival)
+        {
+            case pedra:
+                console.log('Ganhou!');
+                pontosP++;
+                scorePlayer.innerHTML = `Sua pontuação: ${pontosP}`;
+            break;
+            case papel:
+                console.log('empate');
+                pontosB++;
+                pontosP++;
+                scoreBot.innerHTML = `Pontuação da Maqinha: ${pontosB}`;
+                scorePlayer.innerHTML = `Sua pontuação: ${pontosP}`;
+            break;
+            case tesoura:
+                console.log('Perdeu!');
+                pontosB++;
+                scoreBot.innerHTML = `Pontuação da Maqinha: ${pontosB}`;
+            break;
+        }
+    }
+    else if (n === 3) //Tesoura
+    {
+        switch (rival)
+        {
+            case pedra:
+                console.log('Perdeu!');
+                pontosB++;
+                scoreBot.innerHTML = `Pontuação da Maqinha: ${pontosB}`;
+            break;
+            case papel:
+                console.log('Ganhou!');
+                pontosP++;
+                scorePlayer.innerHTML = `Sua pontuação: ${pontosP}`;
+            break;
+            case tesoura:
+                console.log('Empate!');
+                pontosB++;
+                pontosP++;
+                scoreBot.innerHTML = `Pontuação da Maqinha: ${pontosB}`;
+                scorePlayer.innerHTML = `Sua pontuação: ${pontosP}`;
+            break;
+        }
+    }
 
 }
